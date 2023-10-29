71f765eb644b88de6cc9124e92019d8f2e0fe117
Peyman Eskandari <peyman.eskandari@gmail.com>
2023-10-29 09:43:41 +0100

    Add router, radix, tailwin

---
+++ src/Layout.tsx
@@ -0,0 +1,29 @@
+import { Flex, Text, Card, Avatar, Box } from "@radix-ui/themes";
+
+export const Layout = ({ children }: { children?: React.ReactNode }) => {
+  return (
+    <>
+      <Box p="6" className="border-b border-b-gray-200">
+        Header
+      </Box>
+      <Card style={{ maxWidth: 240 }} className="border-blue-500">
+        <Flex gap="3" align="center">
+          <Avatar
+            size="3"
+            src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
+            radius="full"
+            fallback="T"
+          />
+          <Box>
+            <Text as="div" size="2" weight="bold">
+              Teodros Girmay
+            </Text>
+            <Text as="div" size="2" color="gray">
+              Engineering
+            </Text>
+          </Box>
+        </Flex>
+      </Card>
+    </>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { Theme } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
+import "./index.css";
+import { router } from "./router.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Theme>
+      <RouterProvider router={router} />
+    </Theme>
+  </React.StrictMode>
+);
